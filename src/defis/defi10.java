c411ce3e220ca59eb31f3be5e7149a260f92cb0b
nytsanta2038 <nytsanta.andriams@yahoo.fr>
2021-11-12 15:34:22 +0100

    defi_10 in DEV

---
+++ src/defis/defi10.java
@@ -0,0 +1,18 @@
+package defis;
+
+/*
+ * La somme des nombres premiers entre 1 et 10 vaut 2 + 3 + 5 + 7 = 17.
+ *
+ *	Trouver la somme des nombres premiers compris entre 1 et 10'000'000.
+ */
+
+public class defi10 {
+	
+	private static boolean isPremier(int nbre)
+	{
+		boolean bool = false;
+		//TODO
+		return bool;
+	}
+
+}
